6a2f160497e3dbdf4716b71edb7efe76e0142ec1
Theerasak Buakai <theerasak.bu@inet.co.th>
2020-12-01 23:28:08 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/backend', (req, res) => {
-  res.send('Success')
+  res.send('Success Connect Serve Tbnoung')
 })
 
 app.listen(3000, () => {
