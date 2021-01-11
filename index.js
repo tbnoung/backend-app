ecaa7f1cd638649a88d95b3117802c31b2dbc6ba
Theerasak Buakai <theerasak.bu@inet.co.th>
2021-01-11 18:18:47 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-
+var fs = require('fs')
 app.get('/backend', (req, res) => {
+  var data = {
+      code: "200",
+      data: [],
+      message: "save data success",
+      result: "SUCCESS"
+    }
+  // logfile.data.push(data)
+  var savedata = JSON.stringify(data)
+  // console.log('savedata', savedata);
+  fs.appendFileSync('logfile.json', savedata + "\r\n")
   res.send('Success Connect Serve Tbnoung')
 })
 
